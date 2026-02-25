33401ac799794b28336d24b1bea501a926a08938
Briyith sanchez <briyithd.sancheza@uqvirtual.edu.co>
2026-02-24 20:24:27 -0500

    interfaz bicicleta

--- src/main/java/co/edu/uniquindio/poo/model/Bicicleta.java
+++ src/main/java/co/edu/uniquindio/poo/model/Bicicleta.java
@@ -11,13 +11,13 @@ public class Bicicleta {
 
     /**
      * Constructor con los atributos de:
-     * @param color de la bicicleta
-     * @param marca de la bicicleta
-     * @param noSerial de la bicicleta
-     * @param anio antiguedad de la bicicleta
-     * @param tipo de la bicicleta
-     * @param cliente dueño de la bicicleta
-     * @param taller asociado a la bicicleta
+     * @param color de la bicicleta.css
+     * @param marca de la bicicleta.css
+     * @param noSerial de la bicicleta.css
+     * @param anio antiguedad de la bicicleta.css
+     * @param tipo de la bicicleta.css
+     * @param cliente dueño de la bicicleta.css
+     * @param taller asociado a la bicicleta.css
      */
     public Bicicleta(String color, String marca, String noSerial, String anio, TipoBicicleta tipo,
                      Cliente cliente, Taller taller) {
@@ -31,14 +31,14 @@ public class Bicicleta {
     }
 
     /**
-     * @return color de la bicicleta
+     * @return color de la bicicleta.css
      */
     public String getColor() {
         return color;
     }
 
     /**
-     * Metodo para actualizar color de la bicicleta
+     * Metodo para actualizar color de la bicicleta.css
      * @param color
      */
     public void setColor(String color) {
@@ -46,14 +46,14 @@ public class Bicicleta {
     }
 
     /**
-     * @return marca de la bicicleta
+     * @return marca de la bicicleta.css
      */
     public String getMarca() {
         return marca;
     }
 
     /**
-     * Metodo para actualizar marca de la bicicleta
+     * Metodo para actualizar marca de la bicicleta.css
      * @param marca
      */
     public void setMarca(String marca) {
@@ -61,14 +61,14 @@ public class Bicicleta {
     }
 
     /**
-     * @return No. Serial de la bicicleta
+     * @return No. Serial de la bicicleta.css
      */
     public String getNoSerial() {
         return noSerial;
     }
 
     /**
-     * Metodo para actualizar No. Serial de la bicicleta
+     * Metodo para actualizar No. Serial de la bicicleta.css
      * @param noSerial
      */
     public void setNoSerial(String noSerial) {
@@ -76,14 +76,14 @@ public class Bicicleta {
     }
 
     /**
-     * @return años antiguedad de la bicicleta
+     * @return años antiguedad de la bicicleta.css
      */
     public String getAnio() {
         return anio;
     }
 
     /**
-     * Metodo para actualizar años antiguedad de la bicicleta
+     * Metodo para actualizar años antiguedad de la bicicleta.css
      * @param anio
      */
     public void setAnio(String anio) {
@@ -91,14 +91,14 @@ public class Bicicleta {
     }
 
     /**
-     * @return tipo de la bicicleta
+     * @return tipo de la bicicleta.css
      */
     public TipoBicicleta getTipo() {
         return tipo;
     }
 
     /**
-     * Metodo para actualizar el tipo de la bicicleta
+     * Metodo para actualizar el tipo de la bicicleta.css
      * @param tipo
      */
     public void setTipo(TipoBicicleta tipo) {
@@ -106,14 +106,14 @@ public class Bicicleta {
     }
 
     /**
-     * @return cliente dueño de la bicicleta
+     * @return cliente dueño de la bicicleta.css
      */
     public Cliente getCliente() {
         return cliente;
     }
 
     /**
-     * Metodo para actualizar el cliente dueño de la bicicleta
+     * Metodo para actualizar el cliente dueño de la bicicleta.css
      * @param cliente
      */
     public void setCliente(Cliente cliente) {
@@ -128,7 +128,7 @@ public class Bicicleta {
     }
 
     /**
-     * Metodo para actualizar el taller asociado a la bicicleta
+     * Metodo para actualizar el taller asociado a la bicicleta.css
      * @param taller
      */
     public void setTaller(Taller taller) {
